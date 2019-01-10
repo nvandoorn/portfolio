e4208cbe44a1702213de876ef100de1185d8dd2b
Nick Van Doorn <vandoorn.nick@gmail.com>
2019-01-10 15:42:12 -0800

    Use unit from global definition

--- src/components/container.js
+++ src/components/container.js
@@ -6,18 +6,18 @@ const mq = breakpoints
   .map(
     ({ breakpoint, size }) => `
   @media (min-width: ${breakpoint}px) {
-    width: ${size}px;
+    width: ${size};
   }
   `
   )
   .join('\n')
 
-const containerStyle = css(`
+const containerStyle = css`
   display: block;
   margin: 0 auto;
   padding: ${margins.lg}px 0;
   width: 93%;
   ${mq}
-`)
+`
 
 export const Container = p => <div className={containerStyle}>{p.children}</div>
