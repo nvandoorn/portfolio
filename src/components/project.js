267373d7d1805d9a3d1d65cb8559264eed87899f
Nick Van Doorn <vandoorn.nick@gmail.com>
2019-01-10 15:45:54 -0800

    Add url prop & fixup styles

--- src/components/project.js
+++ src/components/project.js
@@ -5,50 +5,71 @@ import { Tag } from './tag'
 import { DefaultButton as Button } from './button'
 import { margins, radius, transitions, breakpoints } from './globals'
 
-const projectHeadingStyle = css(`
-  margin: 0;
-`)
-const projectBriefStyle = css(`
+const projectHeadingStyle = css`
+  margin: 0 0 0 ${margins.sm}px;
+`
+const projectBriefStyle = css`
   margin: 0;
   font-weight: 300;
-`)
+`
 
-const projectBlockStyle = css(`
+const projectBlockStyle = css`
   margin: ${margins.md}px 0;
   display: flex;
   flex-wrap: wrap;
-`)
+`
 
-const imgStyle = css(`
+const imgStyle = css`
   max-height: 85vw;
-`)
+`
 
-const imgContainerStyle = css(`
-`)
+const linkStyle = css`
+  display: flex;
+  align-content: center;
+  padding: ${margins.sm}px 0;
+  border-color: rgba(0, 0, 0, 0);
+  :hover {
+    border-color: #ffffff;
+  }
+`
 
-const descStyle = css(`
+const imgContainerStyle = css``
+
+const descStyle = css`
   font-weight: 300;
   line-height: 2em;
-`)
+`
 
-const projectStyle = css(`
+const projectStyle = css`
   margin: ${margins.lg}px 0;
-`)
+`
 
-const projectHeadingContainerStyle = css(`
+const linkIconStyle = css`
+  display: flex;
+  align-items: center;
+`
+
+const projectHeadingContainerStyle = css`
   display: flex;
   justify-content: space-between;
   align-items: flex-end;
   margin-bottom: ${margins.md}px;
-`)
+  flex-wrap: wrap;
+`
 
-const projectTagContainerStyle = css(`
+const projectTagContainerStyle = css`
   display: flex;
-`)
+  align-items: center;
+  flex-wrap: wrap;
+`
+
+const dateStyle = css`
+  margin-bottom: ${margins.sm}px;
+`
 
 export const Project = p => {
   const marginRight = p.imgUrl ? margins.md : 0
-  const descContainerStyle = css(`
+  const descContainerStyle = css`
     display: flex;
     flex-direction: column;
     justify-content: center;
@@ -57,15 +78,20 @@ export const Project = p => {
       margin: 0 0 0 ${marginRight}px;
       flex: 1;
     }
-  `)
+  `
   return (
     <article className={projectStyle}>
       <div className={projectHeadingContainerStyle}>
         <div className={projectTagContainerStyle}>
-          <h2 className={projectHeadingStyle}>{p.name}</h2>
+          <a className={linkStyle} href={p.url}>
+            <div className={linkIconStyle}>
+              <i className="fa fa-link" />
+              <h2 className={projectHeadingStyle}>{p.name}</h2>
+            </div>
+          </a>
           {p.inProd ? <Tag fontSize="0.8em">In Prod</Tag> : null}
         </div>
-        <p>{p.date}</p>
+        <p className={dateStyle}>{p.date}</p>
       </div>
       <h3 className={projectBriefStyle}>{p.brief}</h3>
       <div className={projectBlockStyle}>
