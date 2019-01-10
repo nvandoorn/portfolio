5df491f0f3e3b6267c5c27299f58bbbb6529ccf0
Nick Van Doorn <vandoorn.nick@gmail.com>
2019-01-10 15:46:13 -0800

    (WIP) Implement about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,49 @@
 import React from 'react'
-import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
+import { AboutEntry } from '../components/about-entry'
 
-export default () => <Layout>About</Layout>
+const aboutLists = [
+  {
+    header: `Values and Ethics`,
+    listItems: [
+      `Above all, we must use technology for good (not to be confused with commercial success)`,
+      'People are most important',
+      'Empower everyone to do amazing work'
+    ]
+  },
+  {
+    header: 'Skills and Experience',
+    listItems: [
+      'Building beautiful user applications using web technology',
+      'Deploying function based infrastructure',
+      'Designing software interfaces between modules',
+      'Orchestrating fleets of cellular devices',
+      'Designing embeded software'
+    ]
+  },
+  {
+    header: 'Tools',
+    listItems: [
+      'Frontend: React, Angular, Ionic, RxJS',
+      'Infrastructure: Node, Firebase, Google Cloud (Cloud Functions, Pubsub, Stackdriver)',
+      'Embedded: C, Linux, Legato, Yocto, AirVantage'
+    ]
+  },
+  {
+    header: `Future Learning Ideas`,
+    listItems: [
+      'Ruby, Go, Rust, Smalltalk, ReasonML',
+      'Linux framebuffer API',
+      'Implement TCP from scratch'
+    ]
+  }
+]
+
+export default () => (
+  <Layout>
+    {aboutLists.map((entry, i) => (
+      <AboutEntry {...entry} key={entry.header} />
+    ))}
+  </Layout>
+)
