232e072df1404d33d2fd10c466a21d0bc87de38a
Nick Van Doorn <vandoorn.nick@gmail.com>
2019-03-06 12:22:21 -0800

    Style AboutEntry component
    Style changes so far are related to spacing.

--- src/components/about-entry.js
+++ src/components/about-entry.js
@@ -1,13 +1,26 @@
 import React from 'react'
 import { css } from 'react-emotion'
+import { margins } from './globals'
 
-export const AboutEntry = p => (
-  <article>
-    <h2>{p.header}</h2>
-    {p.listItems ? (
+const { md, lg } = margins
+
+const aboutListStyle = css`
+  margin: ${lg}px 0 0 0;
+  & ul {
+    list-style-type: none;
+    & li {
+      margin: ${md}px 0;
+    }
+  }
+`
+
+export const AboutEntry = ({ header, listItems }) => (
+  <article className={aboutListStyle}>
+    <h2>{header}</h2>
+    {listItems ? (
       <ul>
-        {p.listItems.map((item, i) => (
-          <li key={`aboutItem${i}`}>{item}</li>
+        {listItems.map((item, i) => (
+          <li key={`about-item-${i}`}>{item}</li>
         ))}
       </ul>
     ) : null}
