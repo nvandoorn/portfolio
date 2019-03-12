683f6742e7aa2bbbb52ef162949ba1edf3be8baf
Nick Van Doorn <vandoorn.nick@gmail.com>
2019-03-11 23:03:58 -0700

    Support height property

--- src/components/container.js
+++ src/components/container.js
@@ -4,20 +4,24 @@ import { breakpoints, margins } from './globals'
 
 const mq = breakpoints
   .map(
-    ({ breakpoint, size }) => `
-  @media (min-width: ${breakpoint}px) {
-    width: ${size};
-  }
-  `
+    ({ breakpoint, size }) => css`
+      @media (min-width: ${breakpoint}px) {
+        width: ${size};
+      }
+    `
   )
   .join('\n')
 
-const containerStyle = css`
-  display: block;
-  margin: 0 auto;
-  padding: ${margins.lg}px 0;
-  width: 93%;
-  ${mq}
-`
-
-export const Container = p => <div css={containerStyle}>{p.children}</div>
+export const Container = p => {
+  const containerStyle = css`
+    display: flex;
+    flex-direction: column;
+    margin: 0 auto;
+    padding: ${margins.lg}px 0;
+    width: 93%;
+    height: ${p.height || '100%'};
+    box-sizing: border-box;
+    ${mq}
+  `
+  return <div css={containerStyle}>{p.children}</div>
+}
