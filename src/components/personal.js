63230329f43fe9fa43422e0771848580148bec0c
Nick Van Doorn <vandoorn.nick@gmail.com>
2019-03-17 23:03:40 -0700

    Apply `border-bottom` to all first children

--- src/components/personal.js
+++ src/components/personal.js
@@ -4,22 +4,16 @@ import { css } from '@emotion/core'
 import { margins } from './globals'
 
 const personalStyle = css`
-  margin: ${margins.lg}px 0 0 0;
-`
-
-const photoStyle = css`
-  margin: ${margins.md}px 0;
-`
-
-const taglineStyle = css`
-  margin: 0 0 ${margins.md}px 0;
+  & > * {
+    margin: 0 0 ${margins.md}px 0;
+  }
 `
 
 export const Personal = ({ name, email, imgUrl, imgAlt, tagline }) => (
   <article css={personalStyle}>
     {name}
-    <img src={imgUrl} alt={imgAlt} css={photoStyle} />
-    <h3 css={taglineStyle}>{tagline}</h3>
+    <img src={imgUrl} alt={imgAlt} />
+    <h3>{tagline}</h3>
     {email}
   </article>
 )
