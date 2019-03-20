345223b17cb4c1bc7f14a4aa125674a2fed068cf
Nick Van Doorn <vandoorn.nick@gmail.com>
2019-03-20 04:15:08 -0700

    Use ie as favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png' // This path is relative to the root
+        icon: 'src/images/ie.png' // This path is relative to the root
         // of the site.
       }
     },
